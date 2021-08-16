66ff875ee8a8d47bb1a982a36b3a3ccbe5db9588
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-17 02:45:47 +0700

    Part 1

--- app/src/main/java/com/example/vsga_mp_catatan_harian/MainActivity.java
+++ app/src/main/java/com/example/vsga_mp_catatan_harian/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.vsga_mp_catatan_harian;
 
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -40,7 +49,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-        D
+
+        Drawable icon = menu.getItem(0).getIcon();
+        icon.mutate();
+
+        icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Tambah")
+                        .setMessage("Aktivitas")
+                        .setView(taskEditText)
+                        .setPositiveButton("Tambah", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dbHelper.insertNewTask(task);
+                                loadTaskList();
+                            }
+                        })
+                        .setNegativeButton("Batala", null)
+                        .create();
+                dialog.show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) findViewById(R.id.task_tittle);
+        String task = String.valueOf(taskTextView.getText());
+        dbHelper.deleteTask(task);
+        loadTaskList();
     }
 }
