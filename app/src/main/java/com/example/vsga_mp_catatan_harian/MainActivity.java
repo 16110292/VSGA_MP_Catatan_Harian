15075115af955104a038821a31640181b6630994
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-17 02:21:31 +0700

    Part 1

--- app/src/main/java/com/example/vsga_mp_catatan_harian/MainActivity.java
+++ app/src/main/java/com/example/vsga_mp_catatan_harian/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.vsga_mp_catatan_harian;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         lstTask = (ListView) findViewById(R.id.lstTask);
 
         loadTaskList();
+    }
+
+    private void loadTaskList() {
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if (mAdapter==null) {
+            mAdapter = new ArrayAdapter<String>(this, R.layout.layout_row, R.id.task_tittle, taskList);
+            lstTask.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        D
+        return super.onCreateOptionsMenu(menu);
     }
 }
